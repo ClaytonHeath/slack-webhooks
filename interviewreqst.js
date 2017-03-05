ec31cdf7eb41deba9396330467284e527b4c17cc
Clayton Moulynox <clayton@auth0.com>
2017-03-04 22:58:50 -0800

    Update interviewreqst.js

--- interviewreqst.js
+++ interviewreqst.js
@@ -93,7 +93,7 @@ function get_details() {/*
             border-radius: 0%;
             box-shadow: 0px 0px 0px 0px #D0D2D3;
             position: absolute;
-            top: 180px;
+            top: 100px;
             right: 45.5%;
         }
 
